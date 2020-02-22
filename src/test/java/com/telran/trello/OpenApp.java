ee03889da6978050f3a3a0e6c4e6bffd1f325599
romanichh <58612871+romanichh@users.noreply.github.com>
2020-02-22 23:20:55 +0200

    created repository Trello-AndroidMobile

---
+++ src/test/java/com/telran/trello/OpenApp.java
@@ -0,0 +1,44 @@
+package com.telran.trello;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class OpenApp {
+    AppiumDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "qa22");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "com.trello");
+        capabilities.setCapability("appActivity", ".home.HomeActivity");
+        capabilities.setCapability("app",
+                "C:/Users/romic/Downloads/Trello_new.apk");
+
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+
+    @Test
+    public void openUp() {
+        System.out.println("Application opened");
+    }
+
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+
+}
